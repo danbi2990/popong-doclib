cbb704c881ebfb1f873f9af83514e518e7852ac8
Lucy Park <me@lucypark.kr>
2014-05-31 15:34:27 +0900

    Change repo name to doclib

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='popong-doclib',
+      version='0.1',
+      description='Team POPONG Document processing Library',
+      url='http://bitbucket.org/teampopong/popong-doclib',
+      author='Team POPONG',
+      author_email='contact@popong.com',
+      license='Apache 2.0',
+      packages=['popong-doclib'],
+      zip_safe=False)
